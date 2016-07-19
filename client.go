e8897213891299276bc2eac4f72c6b8d3b8996f9
Lev Orekhov <lorekhov@yandex-team.ru>
2016-07-19 19:28:07 +0500

    Wrap internal errors into package specific errors

--- client.go
+++ client.go
@@ -8,6 +8,21 @@ import (
 	"errors"
 )
 
+
+var WrongUrlError = errors.New("Wrong url")
+
+
+type RequestError struct {
+	Query string
+	Type string
+}
+
+
+func (e RequestError) Error() string {
+	return e.Type
+}
+
+
 type Client struct {
 	url url.URL
 	client *http.Client
@@ -17,23 +32,40 @@ type Client struct {
 func NewFromString(urlString string) (*Client, error) {
 	url, err := url.Parse(urlString)
 	if err != nil {
-		return nil, err
+		return nil, WrongUrlError
 	}
 	return &Client{*url, &http.Client{}}, nil
 }
 
 
-// TODO: here we must handle different errors and expose our errors
+func (g *Client) errorResponse(r RenderRequest, t string) ([]Series, error) {
+	return []Series{}, RequestError{
+		Type: t,
+		Query: g.queryAsString(r),
+	}
+}
+
+
 func (g *Client) QueryRender(r RenderRequest) ([]Series, error) {
 	response, err := g.client.Get(g.queryAsString(r))
+	if err != nil {
+		return g.errorResponse(r, "Request error")
+	}
 	defer response.Body.Close()
 	if response.StatusCode != http.StatusOK {
-		return nil, errors.New("Wrong response code")
+		return g.errorResponse(r, "Wrong status code")
 	}
 
 	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return g.errorResponse(r, "Can't read response body")
+	}
+
 	metrics, err := unmarshallMetrics(body)
-	return metrics, err
+	if err != nil {
+		return g.errorResponse(r, "Can't unmarshall response")
+	}
+	return metrics, nil
 }
 
 
